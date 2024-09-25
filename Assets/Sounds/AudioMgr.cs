01cde3797df0f9ce811eb0684df33aa505003b7a
Liam Francisco <98508195+francisco-liam@users.noreply.github.com>
2024-09-25 14:12:11 -0700

    sounds added and audio mixer set up

---
+++ Assets/Sounds/AudioMgr.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMgr : MonoBehaviour
+{
+    public static AudioMgr inst;
+
+
+    public AudioMixer mixer;
+
+    [Range(-80.0f, 20.0f)]
+    public float masterVolume;
+    [Range(-80.0f, 20.0f)]
+    public float ambientVolume;
+    [Range(-80.0f, 20.0f)]
+    public float bgmVolume;
+
+    void Awake()
+    {
+        inst = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mixer.SetFloat("MasterVolume", masterVolume);
+        mixer.SetFloat("AmbientVolume", ambientVolume);
+        mixer.SetFloat("BGMVolume", bgmVolume);
+    }
+}
