a173e87a51fa385cb2f51d66aa079e61760ddc85
yatharth1999 <yatharth.vohra@gmail.com>
2024-10-11 01:06:05 -0700

    Changes_Scout

--- Assets/ScoutingMgr.cs
+++ Assets/ScoutingMgr.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,138 +5,131 @@ public class MultiEntityScout : MonoBehaviour
 {
     public float mapSize = 500f;
     public List<Entity> selectedEntities;
+    public LineRenderer lineRenderer;
 
-    private List<Vector3> zoneCenters; // Centers of dynamically generated zones
-    private int currentZoneCount;
+    private List<Vector3> quadrantCenters;
 
     void Start()
     {
-        currentZoneCount = 4; // Minimum of four zones
-        // Render zones initially
+        quadrantCenters = GetQuadrantCenters();
+        DrawQuadrantLines();
     }
 
     void Update()
     {
-        
         if (Input.GetKeyDown(KeyCode.P))
         {
-            zoneCenters = new List<Vector3>(); // Initial empty list for zone centers
-            RenderZones(); 
             Debug.Log("Key Pressed");
             selectedEntities = SelectionMgr.inst.selectedEntities;
-            AssignEntitiesToZonesAndScout();
+            AssignEntitiesToQuadrantsAndScout();
         }
     }
 
-    void AssignEntitiesToZonesAndScout()
+    List<Vector3> GetQuadrantCenters()
     {
-        int numberOfEntities = selectedEntities.Count;
-
-        if (numberOfEntities == 0) return;
-
-        // Always have a minimum of four zones
-        currentZoneCount = Mathf.Max(4, numberOfEntities);
-        zoneCenters = GetDynamicZoneCenters(currentZoneCount);
-
-        Vector3 centerPosition = Vector3.zero; // Central point of the map
-
-        for (int i = 0; i < numberOfEntities; i++)
+        return new List<Vector3>
         {
-            Entity entity = selectedEntities[i];
+            new Vector3(mapSize / 2, 0, mapSize / 2),
+            new Vector3(mapSize / 2, 0, -mapSize / 2),
+            new Vector3(-mapSize / 2, 0, -mapSize / 2),
+            new Vector3(-mapSize / 2, 0, mapSize / 2)
+        };
+    }
+
+    void DrawQuadrantLines()
+    {
+        if (lineRenderer == null)
+        {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+        }
+
+        lineRenderer.positionCount = 5;
+        lineRenderer.loop = true;
+        lineRenderer.startWidth = 0.2f;
+        lineRenderer.endWidth = 0.2f;
+
+        Vector3[] points =
+        {
+            new Vector3(mapSize / 2, 0, mapSize / 2),
+            new Vector3(mapSize / 2, 0, -mapSize / 2),
+            new Vector3(-mapSize / 2, 0, -mapSize / 2),
+            new Vector3(-mapSize / 2, 0, mapSize / 2),
+            new Vector3(mapSize / 2, 0, mapSize / 2)
+        };
+
+        lineRenderer.SetPositions(points);
+    }
+
+    void AssignEntitiesToQuadrantsAndScout()
+    {
+        if (selectedEntities.Count == 0) return;
+
+        int totalEntities = selectedEntities.Count;
+        int entityIndex = 0;
+
+        foreach (Entity entity in selectedEntities)
+        {
             UnitAI unitAI = entity.GetComponent<UnitAI>();
             unitAI.StopAndRemoveAllCommands();
-
-            // Assign entity to scout its assigned zones
-            List<Vector3> targetZones = GetTargetZones(i, numberOfEntities);
-            foreach (Vector3 targetZone in targetZones)
-            {
-                Move moveToZone = new Move(entity, targetZone);
-                unitAI.AddCommand(moveToZone);
-            }
-
-            // Return to the original position
             Vector3 originalPosition = entity.position;
-            Move returnToOriginal = new Move(entity, originalPosition);
-            unitAI.AddCommand(returnToOriginal);
+
+            List<Vector3> targetQuadrants = GetTargetQuadrantsForEntity(entityIndex, totalEntities);
+
+            foreach (Vector3 targetQuadrant in targetQuadrants)
+            {
+                Vector3 initialMovePosition = GetInitialMovePosition(entityIndex);
+                Vector3 finalMovePosition = GetFinalMovePosition(entityIndex);
+
+                unitAI.AddCommand(new Move(entity, initialMovePosition));
+                unitAI.AddCommand(new Move(entity, targetQuadrant));
+                unitAI.AddCommand(new Move(entity, finalMovePosition));
+            }
+
+            unitAI.AddCommand(new Move(entity, originalPosition));
+            entityIndex++;
         }
     }
 
-    List<Vector3> GetDynamicZoneCenters(int numberOfZones)
+    List<Vector3> GetTargetQuadrantsForEntity(int entityIndex, int totalEntities)
     {
-        List<Vector3> centers = new List<Vector3>();
+        List<Vector3> targetQuadrants = new List<Vector3>();
+        int quadrantsCount = quadrantCenters.Count;
+        targetQuadrants.Clear();
+        int quadrantsPerEntity = Mathf.CeilToInt((float)quadrantsCount / totalEntities);
 
-        // Divide the map into a grid with at least 4 zones
-        int rows = Mathf.CeilToInt(Mathf.Sqrt(numberOfZones));
-        int columns = Mathf.CeilToInt((float)numberOfZones / rows);
-        float zoneWidth = mapSize / columns;
-        float zoneHeight = mapSize / rows;
-
-        for (int row = 0; row < rows; row++)
+        for (int i = 0; i < quadrantsPerEntity; i++)
         {
-            for (int column = 0; column < columns; column++)
+            int quadrantIndex = entityIndex * quadrantsPerEntity + i;
+            if (quadrantIndex < quadrantsCount)
             {
-                if (centers.Count >= numberOfZones)
-                    break;
-
-                float xOffset = (column * zoneWidth) - (mapSize / 2) + (zoneWidth / 2);
-                float zOffset = (row * zoneHeight) - (mapSize / 2) + (zoneHeight / 2);
-                centers.Add(new Vector3(xOffset, 0, zOffset));
+                targetQuadrants.Add(quadrantCenters[quadrantIndex]);
             }
         }
 
-        return centers;
+        return targetQuadrants;
     }
 
-    List<Vector3> GetTargetZones(int entityIndex, int totalEntities)
+    Vector3 GetInitialMovePosition(int index)
     {
-        List<Vector3> targetZones = new List<Vector3>();
-
-        // Calculate zones for each entity, ensuring all entities are assigned evenly
-        int zonesPerEntity = Mathf.CeilToInt((float)currentZoneCount / totalEntities);
-
-        for (int i = 0; i < zonesPerEntity; i++)
+        switch (index % quadrantCenters.Count)
         {
-            int zoneIndex = (entityIndex * zonesPerEntity + i) % currentZoneCount;
-            targetZones.Add(zoneCenters[zoneIndex]);
-        }
-
-        return targetZones;
-    }
-
-    void RenderZones()
-    {
-        zoneCenters = GetDynamicZoneCenters(currentZoneCount);
-
-        foreach (var zoneCenter in zoneCenters)
-        {
-            CreateZoneRenderer(zoneCenter);
+            case 0: return new Vector3(0, 0, mapSize / 2);
+            case 1: return new Vector3(mapSize / 2, 0, 0);
+            case 2: return new Vector3(0, 0, -mapSize / 2);
+            case 3: return new Vector3(-mapSize / 2, 0, 0);
+            default: return Vector3.zero;
         }
     }
 
-    void CreateZoneRenderer(Vector3 center)
+    Vector3 GetFinalMovePosition(int index)
     {
-        float zoneWidth = mapSize / Mathf.CeilToInt(Mathf.Sqrt(currentZoneCount));
-        float zoneHeight = mapSize / Mathf.CeilToInt((float)currentZoneCount / Mathf.CeilToInt(Mathf.Sqrt(currentZoneCount)));
-
-        Vector3[] corners = new Vector3[5];
-        corners[0] = new Vector3(center.x - zoneWidth / 2, 0, center.z - zoneHeight / 2);
-        corners[1] = new Vector3(center.x + zoneWidth / 2, 0, center.z - zoneHeight / 2);
-        corners[2] = new Vector3(center.x + zoneWidth / 2, 0, center.z + zoneHeight / 2);
-        corners[3] = new Vector3(center.x - zoneWidth / 2, 0, center.z + zoneHeight / 2);
-        corners[4] = corners[0]; // Close the loop
-
-        GameObject lineObj = new GameObject("Zone Border");
-        LineRenderer lineRenderer = lineObj.AddComponent<LineRenderer>();
-
-        lineRenderer.startWidth = 2f;
-        lineRenderer.endWidth = 2f;
-        lineRenderer.positionCount = corners.Length;
-        lineRenderer.useWorldSpace = true;
-
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.startColor = Color.green;
-        lineRenderer.endColor = Color.green;
-
-        lineRenderer.SetPositions(corners);
+        switch (index % quadrantCenters.Count)
+        {
+            case 0: return new Vector3(mapSize / 2, 0, 0);
+            case 1: return new Vector3(0, 0, -mapSize / 2);
+            case 2: return new Vector3(-mapSize / 2, 0, 0);
+            case 3: return new Vector3(0, 0, mapSize / 2);
+            default: return Vector3.zero;
+        }
     }
 }
