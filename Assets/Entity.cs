3a02771639913fb7f961fe1bd4eae0367decbee1
Sushil Louis <sushil.louis@gmail.com>
2024-11-13 15:31:54 -0800

    Compute fuel remaining and range in Entity.cs
    This is for DDG51 and AntiShipMissile only until the Entity data is updated with MaxFuel and MaxRange for every other entity

--- Assets/Entity.cs
+++ Assets/Entity.cs
@@ -40,6 +40,8 @@ public class Entity : MonoBehaviour
     public float desiredHeading; //degrees
     public float health;
     public float fuel;
+    public float range;
+    public float fuelBurnRate;
 
     [Header("Const values")]
     //------------------------------
@@ -49,10 +51,14 @@ public class Entity : MonoBehaviour
     public float turnRate;
     public float maxSpeed;
     public float minSpeed;
+    public float cruiseSpeed;
     public float mass;
     public float length;
     public float width;
     public float height;
+
+    public float maxFuel;
+    public float maxRange;
 
     public EntityType entityType;
 
@@ -67,10 +73,29 @@ public class Entity : MonoBehaviour
         isSelected = false;
         //cameraRig = transform.Find("CameraRig").gameObject;
         //selectionCircle = transform.Find("Decorations").Find("SelectionCylinder").gameObject;
+        fuel = maxFuel;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void FixedUpdate() {
+        ComputeFuelRange();
+    }
+
+    void ComputeFuelRange() {
+        if(speed <= cruiseSpeed) {
+            fuelBurnRate = 1f - (cruiseSpeed - speed) / (cruiseSpeed + 0.0001f);
+        } else {
+            fuelBurnRate = 1f + (speed - cruiseSpeed) / (maxSpeed - cruiseSpeed + 0.0001f) ;
+        }
+        fuel -= fuelBurnRate * Time.fixedDeltaTime * Time.timeScale;
+        fuel = Mathf.Clamp(fuel, 0, maxFuel);
+        range = Mathf.Clamp(fuel * cruiseSpeed, 0, maxRange);
+
     }
 }
--- Assets/UI/UIMgr.cs
+++ Assets/UI/UIMgr.cs
@@ -137,6 +137,10 @@ public class UIMgr : MonoBehaviour
         #endif
     }
     public TextMeshProUGUI entityName;
+
+    public TextMeshProUGUI fuel;
+    public TextMeshProUGUI range;
+
     public TextMeshProUGUI speed;
     public TextMeshProUGUI desiredSpeed;
     public TextMeshProUGUI heading;
@@ -148,6 +152,7 @@ public class UIMgr : MonoBehaviour
     public TextMeshProUGUI target;
     public TextMeshProUGUI timeOnTarget;
     public TextMeshProUGUI targetRange;
+
 
     // Update is called once per frame
     void Update()
@@ -167,6 +172,9 @@ public class UIMgr : MonoBehaviour
                 altitude.text = phx3d.altitude.ToString("F2") + "m";
                 desiredAltitude.text = phx3d.desiredAltitude.ToString("F2") + "m";
             }
+
+            fuel.text = ent.fuel.ToString("F0");
+            range.text = (ent.range * Utils.ToNautialMiles).ToString("F1") + " nm";
 
 
         }
