d003c38ff62c2b8ce76d32e14f969cf05210864a
Sushil Louis <sushil.louis@gmail.com>
2024-11-20 19:57:00 -0800

    Added ugly prototype heading control widget to entity panel.

--- Assets/Scripts/OpenOceanMain.cs
+++ Assets/Scripts/OpenOceanMain.cs
@@ -7,7 +7,8 @@ public class OpenOceanMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameMgr.inst.InitOpenOceanMap();
+        //GameMgr.inst.InitOpenOceanMap();
+        GameMgr.inst.InitTestWidgetMap();
     }
 
     // Update is called once per frame
