6a53c5ff4774880c5981442e8538e98dbc2496ab
yatharth1999 <yatharth.vohra@gmail.com>
2025-02-12 17:14:23 -0800

    added_terrain

--- Assets/Scripts/GameMgr.cs
+++ Assets/Scripts/GameMgr.cs
@@ -139,7 +139,7 @@ public class GameMgr : MonoBehaviour
 
     public void OpenOcean1x1()
     {
-        Vector3 posPlayer1 = new Vector3(0, 0, 0);
+        Vector3 posPlayer1 = new Vector3(0100, 0, -7000);
         Vector3 posPlayer2 = new Vector3(0, 0, 1 * Utils.FromNauticalMiles);
         // MakeEntsForPlayer(posPlayer1, 0, PlayerMgr.inst.player1);
         // MakeEntsForPlayer(posPlayer2, 180, PlayerMgr.inst.player2);
